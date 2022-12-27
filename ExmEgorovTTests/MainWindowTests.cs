3c3aa85db8184a5dcb2e53b8b9095d7e3416c8e7
artem_yarosh <03.artem.yarosh@gmail.com>
2022-12-27 22:37:53 +0300

    add-all

---
+++ ExmEgorovTTests/MainWindowTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExmEgorovT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExmEgorovT.Tests
+{
+    [TestClass()]
+    public class MainWindowTests
+    {
+        [TestMethod()]
+        public void AutorizationTestTest()
+        {
+            
+        }
+
+        [TestMethod()]
+        public void AutorizationTestTest1()
+        {
+            string login = "dlfj";
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AutorizationTestTest2()
+        {
+            string login = "dlf9j";
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AutorizationTestTest3()
+        {
+
+        }
+
+        [TestMethod()]
+        public void AutorizationTestTest4()
+        {
+
+        }
+    }
+}
